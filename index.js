9e847f108de461ab73d6cc48567609cd0a94f808
Uriel Huertas <pryonx8@gmail.com>
2018-09-10 11:11:04 +0200

    using wolf global paths

--- index.js
+++ index.js
@@ -1,12 +1,67 @@
 const jscpd = require('jscpd');
 const path = require('path');
-const rootPath = path.join(__dirname, '..', '..');
-const {WolfLinter, WolfLinterError} = require(path.join(rootPath, 'bin', 'wolflinter'));
+const { WolfLinter } = require(path.join(binPath, 'wolflinter'));
+
+class WolfDupeError {
+
+    constructor(lines, tokens, firstFile, secondFile, fragment) {
+        this.lines = lines;
+        this.tokens = tokens;
+        this.firstFile = firstFile;
+        this.secondFile = secondFile;
+        this.fragment = fragment;
+    }
+}
 
 const _private = new WeakMap();
 class WolfJSCPD extends WolfLinter {
+
     constructor() {
         super();
+
+        this.options = {
+            path: rootPath,
+            files: '**/*.ts',
+            exclude: ['**/node_modules/**'],
+            reporter: 'json',
+            silent: true
+        };
+
+        _private.set(this, {
+            /**
+             * @desc {Function} _processDupeCheckResults
+             * @param lines
+             * @param tokens
+             * @param firstFile
+             * @param secondFile
+             * @param fragment
+             * @private
+             */
+            _processDupeCheckResults: ({lines, tokens, firstFile, secondFile, fragment}) => {
+                return new WolfDupeError(lines, tokens, firstFile, secondFile, fragment);
+            },
+            /** @private
+             *  @override
+             *  {Array<WolfDupeError>>} _errors
+             **/
+            _errors: []
+        })
+    }
+
+    track() {
+
+        const result = new jscpd().run(this.options);
+        result.report.duplicates.map(_private.get(this)._processDupeCheckResults)
+                                .forEach((dupeErr) => this.addErrors(dupeErr));
+    }
+
+    addErrors(err) {
+        const _errors = _private.get(this)._errors;
+        _errors.push(err);
+    }
+
+    getErrors() {
+        return _private.get(this)._errors;
     }
 }
 
